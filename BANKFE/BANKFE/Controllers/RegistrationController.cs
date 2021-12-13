dee2087e7c312e5938cc8c61e6da702137b813fa
dankusuma <kusumayudha32@gmail.com>
2021-12-13 11:23:09 +0800

    Registration Form Step 1 & 2 FrontEndChange API Url

---
+++ BANKFE/BANKFE/Controllers/RegistrationController.cs
@@ -0,0 +1,31 @@
+﻿using BANKFE.Models;
+using BANKFE.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BANKFE.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private readonly HttpService _httpservices;
+        private readonly IConfiguration _configuration;
+
+        public RegistrationController(HttpService httpservice, IConfiguration configuration)
+        {
+            _httpservices = httpservice;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitRegistration([FromBody] Registration model)
+        {
+            var result = _httpservices.PostData(_configuration["APIUrl"] + "/User/Add", model);
+            return Ok(result.Result);
+        }
+    }
+}

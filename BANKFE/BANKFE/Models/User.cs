782d3c2bf9d4eb3a58165a290067d6e941eb86b0
HendraaPr <96100882+HendraaPr@users.noreply.github.com>
2021-12-14 14:52:44 +0700

    New Feature, Register User

---
+++ BANKFE/BANKFE/Models/User.cs
@@ -0,0 +1,24 @@
+﻿namespace BANKFE.Models
+{
+    public class User
+    {
+        public int USER_ID { get; set; }
+        public string USERNAME { get; set; }
+        public string PASSWORD { get; set; }
+        public string PIN { get; set; }
+        public string NAME { get; set; }
+        public string ADDRESS { get; set; }
+        public string PHONE { get; set; }
+        public string FOTO_KTP { get; set; }
+        public string FOTO_SELFIE { get; set; }
+
+        public bool IS_VALIDATE { get; set; }
+
+        public string LOGIN_HOLD { get; set; }
+
+
+        public int LOGIN_FAILED { get; set; }
+
+        public string USER_TYPE { get; set; }
+    }
+}

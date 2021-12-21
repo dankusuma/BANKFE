30b3f3b1962b8d92ff652dbf830c28e813a870f5
dankusuma <kusumayudha32@gmail.com>
2021-12-21 09:39:50 +0800

    Login Feature

--- BANKFE/BANKFE/Controllers/LoginController.cs
+++ BANKFE/BANKFE/Controllers/LoginController.cs
@@ -1,7 +1,15 @@
 ﻿using BANKFE.Models;
 using BANKFE.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace BANKFE.Controllers
 {
@@ -21,11 +29,40 @@ namespace BANKFE.Controllers
         }
 
         [HttpPost]
-        public IActionResult DoLogin([FromBody] Login login)
+        public async Task<IActionResult> DoLoginAsync([FromBody] Login login)
         {
             var result = _httpservices.PostData(_configuration["APIUrl"] + "/User/Authenticate", login);
-            return Ok(result.Result);
+            if(result.Result.Code != 200)
+            {
+                return Unauthorized(result.Result.Message);
+            }
+            var token = new JwtSecurityToken(jwtEncodedString: result.Result.Message);
+            var claims = new List<Claim>() {
+                        new Claim(ClaimTypes.Name,token.Claims.First(c => c.Type == ClaimTypes.Name).Value),
+                        new Claim(ClaimTypes.Role, token.Claims.First(c => c.Type == ClaimTypes.Role).Value),
+                        new Claim(ClaimTypes.Authentication, result.Result.Message)
+
+            };
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            //Initialize a new instance of the ClaimsPrincipal with ClaimsIdentity    
+            var principal = new ClaimsPrincipal(identity);
+            //SignInAsync is a Extension method for Sign in a principal for the specified scheme.    
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties()
+            {
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes((token.ValidTo - token.ValidFrom).TotalMinutes),
+                IsPersistent = true
+            }) ;
+            return Ok();
         }
+
+        public async Task<IActionResult> LogOut()
+        {
+            //SignOutAsync is Extension method for SignOut    
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            //Redirect to home page    
+            return LocalRedirect("/");
+        }
+
 
     }
 }

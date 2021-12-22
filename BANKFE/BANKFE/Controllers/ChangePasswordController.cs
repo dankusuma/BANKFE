8cc2d6a8c344b84eb4261ca3168d8369fc9f2f22
dankusuma <kusumayudha32@gmail.com>
2021-12-22 12:40:08 +0800

    Change Password Feature

---
+++ BANKFE/BANKFE/Controllers/ChangePasswordController.cs
@@ -0,0 +1,46 @@
+﻿using BANKFE.Services;
+using BANKFE.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace BANKFE.Controllers
+{
+    public class ChangePasswordController : Controller
+    {
+
+        private readonly HttpService _httpservices;
+        private readonly IConfiguration _configuration;
+        public ChangePasswordController(HttpService httpservice, IConfiguration configuration)
+        {
+            _httpservices = httpservice;
+            _configuration = configuration;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DoChangePassword([FromBody] ChangePasswordViewModel param)
+        {
+            var result = await _httpservices.PostData(_configuration["APIUrl"] + "/User/Authenticate", param);
+            if ((int)result.StatusCode != 200)
+            {
+                return Unauthorized(result.Content.ReadAsStringAsync());
+            }
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SendChangePassword([FromQuery] string username)
+        {
+            var result = await _httpservices.GetData(_configuration["APIUrl"] + "/User/Authenticate");
+            return Ok();
+        }
+
+
+    }
+}

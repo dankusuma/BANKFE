6fd24fa38341c982292efcd45c8ad2bf995bee8f
MITRAIS\Romario_P852 <Romario_P852@mitrais.com>
2022-01-07 09:36:59 +0700

    Create PIN Feature - Add feature such as PIN validation if no PIN created yet then redirect to create PIN page or redirect to dashboard

--- BANKFE/BANKFE/Controllers/HomeController.cs
+++ BANKFE/BANKFE/Controllers/HomeController.cs
@@ -2,11 +2,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace BANKFE.Controllers
 {
@@ -22,7 +20,12 @@ namespace BANKFE.Controllers
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+            var user = User as ClaimsPrincipal;
+            string username = user.Claims.Where(c => c.Type == ClaimTypes.Name).Select(x => x.Value).FirstOrDefault();
+            string pinStatus = user.Claims.Where(c => c.Type == "pin_status").Select(x => x.Value).FirstOrDefault();
+
+            if (pinStatus == "true") return View();  /// Redirect to dashboard
+            else return RedirectToAction("Index", "Pin", new { username = username });   /// Redirect to PIN page
         }
 
         public IActionResult NotReadyPage()

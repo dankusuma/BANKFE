3d7369b3c2315aacf3a3953859eafdddb1778274
Saiful Setiawan <saiful.setiawan@mitrais.com>
2022-01-10 13:10:42 +0700

    add logic to disable send email button when the inputted email isnt valid

---
+++ BANKFE/BANKFE/wwwroot/js/pages/email-validation.js
@@ -0,0 +1,7 @@
+﻿const isEmailValid = (email) => {
+    const pattern = /^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    const isValid = pattern.test(email);
+    //console.log('is email ' + email + ' valid: ' + isValid);
+
+    return isValid;
+}

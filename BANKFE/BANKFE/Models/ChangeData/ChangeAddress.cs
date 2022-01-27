464118a1a11af03a572cfbec2ac0c483604adc83
Saiful Setiawan <saiful.setiawan@mitrais.com>
2022-01-27 09:52:24 +0700

    create model class for change address

---
+++ BANKFE/BANKFE/Models/ChangeData/ChangeAddress.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BANKFE.Models.ChangeData
+{
+    public class ChangeAddress
+    {
+        [Display(Name = "Username")]
+        public string USERNAME { get; set; }
+
+        [Display(Name = "Alamat")]
+        [Required(ErrorMessage = "Alamat tidak boleh kosong")]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = "Panjang tidak boleh lebih dari 200 karakter")]
+        public string ADDRESS { get; set; }
+
+        [Display(Name = "Provinsi")]
+        [Required(ErrorMessage = "Provinsi tidak boleh kosong")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Panjang tidak boleh lebih dari 50 karakter")]
+        public string PROVINCE { get; set; }
+
+        [Display(Name = "Kabupaten/Kota")]
+        [Required(ErrorMessage = "Kabupaten tidak boleh kosong")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Panjang tidak boleh lebih dari 50 karakter")]
+        public string KABUPATEN_KOTA { get; set; }
+
+        [Display(Name = "Kecamatan")]
+        [Required(ErrorMessage = "Kecamatan tidak boleh kosong")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Panjang tidak boleh lebih dari 50 karakter")]
+        public string KECAMATAN { get; set; }
+
+        [Display(Name = "Kelurahan")]
+        [Required(ErrorMessage = "Kelurahan tidak boleh kosong")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Panjang tidak boleh lebih dari 50 karakter")]
+        public string KELURAHAN { get; set; }
+    }
+}

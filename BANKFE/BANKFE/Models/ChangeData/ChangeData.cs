51ed366a507c64b9508344ed51df2bb9fb290a22
Saiful Setiawan <saiful.setiawan@mitrais.com>
2022-01-28 08:17:09 +0700

    create new class for containing both change job and change address

---
+++ BANKFE/BANKFE/Models/ChangeData/ChangeData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BANKFE.Models.ChangeData
+{
+    public class ChangeData
+    {
+        public string USERNAME { get; set; }
+
+        public string ADDRESS { get; set; }
+        public string PROVINCE { get; set; }
+        public string KABUPATEN_KOTA { get; set; }
+        public string KECAMATAN { get; set; }
+        public string KELURAHAN { get; set; }
+        public string JOB { get; set; }
+    }
+}
